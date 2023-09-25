fea490a101204795afdca34d981b814da6b2f8f3
reyes122 <bryanibarra959@gmail.com>
2023-09-25 11:54:30 -0600

    Unidad1

---
+++ Unidad1/Unidad1_Modulo1/Ejercicio-1.py
@@ -0,0 +1,10 @@
+'''
+ Nombre: Bryan Adrian Reyes Ibarra
+ Fecha: 25 de Septiembre 
+ Description:
+ Problema 1
+ Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética 
+'''
+
+resultado = ((3+2)/(2*5))**2
+print("El resultado es =", resultado)
---
+++ Unidad1/Unidad1_Modulo1/Ejercicio-2.py
@@ -0,0 +1,11 @@
+'''
+ Nombre: Bryan Adrian Reyes Ibarra
+ Fecha: 25 de Septiembre 
+ Description:
+ Problema 2
+ Escribir un programa que lea un entero positivo, n, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n. La suma de los n primeros enteros positivos puede ser calculada de la siguiente forma:
+'''
+print("Dame un numero esto positivo")
+n = int(input())
+suma = (n(n+1))/2
+print("Resultado de la suma ", suma)
